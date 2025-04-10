14640f174553a7ee42e363cf4ccebcbf2566811e
pyjhoop <pyjhoop@naver.com>
2025-04-10 20:12:05 +0900

    인프런: 4. 단어 뒤집기
    - StringBuilder의 reverse()로 간단히 뒤집을 수 있음.
    
    - 투 포인터로 lt, rt를 잡고 한칸씩 이동하면서 서로 값을 교환하여 뒤집을 수도 있음.

---
+++ src/main/java/org/algorithm/inflearn/string/단어뒤집기_5.java
@@ -0,0 +1,36 @@
+package org.algorithm.inflearn.string;
+
+import java.io.*;
+public class 단어뒤집기_5 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            String s = br.readLine();
+            s = solution(s);
+            result.append(s).append("\n");
+        }
+
+        System.out.println(result);
+
+
+    }
+
+    public static String solution(String str) {
+
+        char[] chars = str.toCharArray();
+        int lt = 0;
+        int rt = str.length() - 1;
+
+        while(lt < rt) {
+            char temp = chars[lt];
+            chars[lt] = chars[rt];
+            chars[rt] = temp;
+            lt++;
+            rt--;
+        }
+        return new String(chars);
+    }
+}
